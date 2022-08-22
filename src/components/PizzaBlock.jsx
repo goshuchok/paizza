66cc8df52a7d5223756955242cb6c840f37dd661
Igor Povar <goshuchok@gmail.com>
2022-08-22 10:36:06 +0300

    sort popup

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -16,13 +16,13 @@ function PizzaBlock({ title, price, imageUrl, sizes, types }) {
       <h4 className="pizza-block__title">{title}</h4>
       <div className="pizza-block__selector">
         <ul>
-          {types.map((type, i) => (
+          {types.map((typeId) => (
             <li
-              onClick={() => setActiveType(i)}
-              className={activeType === i ? 'active' : ''}
-              key={i}
+              onClick={() => setActiveType(typeId)}
+              className={activeType === typeId ? 'active' : ''}
+              key={typeId}
             >
-              {typeNames[type]}
+              {typeNames[typeId]}
             </li>
           ))}
         </ul>
